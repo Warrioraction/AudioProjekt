3aad868e5fd8d1d079a9910ec47ab3e031a08776
Attia <med3attia@live.fr>
2021-01-27 16:09:43 +0100

    Menus mit Scripts

---
+++ Jump'n'Run/Assets/Scripts/SC_Settings.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Settings : MonoBehaviour
+{
+    public GameObject Settings;
+    public GameObject Help;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SettingsButton();
+    }
+
+    public void BackSettingsButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+     
+
+    public void HelpButton()
+    {
+        // Show Credits Menu
+        Settings.SetActive(false);
+        Help.SetActive(true);
+    }
+
+    public void SettingsButton()
+    {
+        // Show Main Menu
+        Settings.SetActive(true);
+        Help.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        // Quit Game
+        Application.Quit();
+    }
+}
