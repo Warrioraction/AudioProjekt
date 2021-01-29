f0091d47dd94d845c3c3b1dc23a2de83b8b5c261
Lukas Liu <lukasliu@web.de>
2021-01-29 23:59:51 +0100

    Forest level design concept

---
+++ Jump'n'Run/Assets/Scripts/Level/MovingPlatform.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{ 
+    public float speed;
+    public float distance;
+    public bool inverted;
+    private float xStartPosition;
+  
+    void Start () {
+        xStartPosition = transform.position.x;
+    }
+    void Update () {
+        if (!inverted)
+        {
+            if ((speed < 0 && transform.position.x < xStartPosition) ||
+                (speed > 0 && transform.position.x > xStartPosition + distance))
+            {
+                speed *= -1;
+            }
+        }
+        else
+        {
+            if ((speed > 0 && transform.position.x > xStartPosition) ||
+                (speed < 0 && transform.position.x < xStartPosition - distance))
+            {
+                speed *= -1;
+            }
+        }
+
+        transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+    }
+    
+}
