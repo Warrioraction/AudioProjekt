49b8d2f6ccf1ca6ff0cabc27dddfa42bb9670872
Attia <med3attia@live.fr>
2021-02-02 01:26:43 +0100

    Pause Button fixed , sliders in the pause menu do work, Sliders in the settings menu should be working, Start button sound

---
+++ Jump'n'Run/Assets/Scripts/Menus/SetVolume.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour {
+   public Slider volumeSlider;
+    public AudioMixer mixer;
+
+    public void SetLevel ()
+    {
+        mixer.SetFloat("BackgroundVol", volumeSlider.value * 20);
+    }
+    public void Mute ()
+    {
+        volumeSlider.value =-1;    }
+      public void Max ()
+    {
+        volumeSlider.value =1;    }
+            public void SetSFX ()
+    {
+        mixer.SetFloat("SFXVol", volumeSlider.value * 20);
+    }
+    
+}
